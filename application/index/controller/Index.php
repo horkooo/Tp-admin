72d69605b62dbf78b423c40113ede84808e3823b
云淡风轻 <2398156504@qq.com>
2018-10-17 11:53:50 +0800

    Upload Index.php

---
+++ application/index/controller/Index.php
@@ -0,0 +1,46 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2017 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://think.ctolog.com
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | github开源项目：https://github.com/zoujingli/ThinkAdmin
+// +----------------------------------------------------------------------
+
+namespace app\index\controller;
+
+use service\SmsService;
+use think\Controller;
+
+/**
+ * 网站入口控制器
+ * Class Index
+ * @package app\index\controller
+ * @author Anyon <zoujingli@qq.com>
+ * @date 2017/04/05 10:38
+ */
+class Index extends Controller
+{
+
+    /**
+     * 网站入口
+     */
+    public function index()
+    {
+        $this->redirect('@admin');
+		exit;
+    }
+
+    public function test(){
+        $aa = SmsService::send('15225098797','reg','111222');
+        echo "<pre>";
+        print_r($aa);
+        exit;
+    }
+
+}
