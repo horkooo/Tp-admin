53f53adf4c94503c3005582b19ac22361f78409c
云淡风轻 <2398156504@qq.com>
2019-05-16 18:44:33 +0800

    更新 ToolsService.php 修复前端接口跨域失败的问题

--- extend/service/ToolsService.php
+++ extend/service/ToolsService.php
@@ -1,17 +1,5 @@
 <?php
 
-// +----------------------------------------------------------------------
-// | ThinkAdmin
-// +----------------------------------------------------------------------
-// | 版权所有 2014~2017 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
-// +----------------------------------------------------------------------
-// | 官方网站: http://think.ctolog.com
-// +----------------------------------------------------------------------
-// | 开源协议 ( https://mit-license.org )
-// +----------------------------------------------------------------------
-// | github开源项目：https://github.com/zoujingli/ThinkAdmin
-// +----------------------------------------------------------------------
-
 namespace service;
 
 use think\Request;
@@ -41,8 +29,12 @@ class ToolsService
             header('Content-Type:text/plain charset=UTF-8');
             header('Content-Length: 0', true);
             header('status: 204');
-            header('HTTP/1.0 204 No Content');
-            exit;
+            header('HTTP/1.0 204 No Content');          
+        }else{
+            header('Access-Control-Allow-Origin:*');
+            header('Access-Control-Allow-Headers:Accept,Referer,Host,Keep-Alive,User-Agent,X-Requested-With,Cache-Control,Content-Type,Cookie,token');
+            header('Access-Control-Allow-Credentials:true');
+            header('Access-Control-Allow-Methods:GET,POST,OPTIONS');
         }
     }
 
