dc5904228f5aebd2dd2d94f4efcff4c3110d2fed
云淡风轻 <2398156504@qq.com>
2019-07-24 19:13:24 +0800

    新增图片压缩

--- extend/service/ToolsService.php
+++ extend/service/ToolsService.php
@@ -235,6 +235,62 @@ class ToolsService
         return $result;
     }
 
+    /*
+     * 压缩图像
+     * @param string $imgsrc
+     * @param string $imgdst
+     * @return string
+     */
+    public static function compressedImage($imgsrc,$imgdst) {
+        list($width, $height, $type) = getimagesize($imgsrc);
+
+        $new_width = $width;//压缩后的图片宽
+        $new_height = $height;//压缩后的图片高
+
+        if($width >= 600){
+            $per = 600 / $width;//计算比例
+            $new_width = $width * $per;
+            $new_height = $height * $per;
+        }
+
+        switch ($type) {
+            case 1:
+                $giftype = check_gifcartoon($imgsrc);
+                if ($giftype) {
+                    $image_wp = imagecreatetruecolor($new_width, $new_height);
+                    $image = imagecreatefromgif($imgsrc);
+                    imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                    //90代表的是质量、压缩图片容量大小
+                    imagejpeg($image_wp, $imgdst, 90);
+                    imagedestroy($image_wp);
+                    imagedestroy($image);
+                }
+                ob_end_flush();
+                break;
+            case 2:
+                $image_wp = imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefromjpeg($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                //90代表的是质量、压缩图片容量大小
+                imagejpeg($image_wp, $imgdst, 90);
+                imagedestroy($image_wp);
+                imagedestroy($image);
+                ob_end_flush();
+                break;
+            case 3:
+                $image_wp = imagecreatetruecolor($new_width, $new_height);
+                $image = imagecreatefrompng($imgsrc);
+                imagecopyresampled($image_wp, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                //90代表的是质量、压缩图片容量大小
+                imagejpeg($image_wp, $imgdst, 90);
+                imagedestroy($image_wp);
+                imagedestroy($image);
+                break;
+        }
+        return $imgdst;
+    }
+
+
 
 
 
